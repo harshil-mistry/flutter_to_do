d071d6d12723afe17e02bbb0cf859ed307b07c52
harshil-mistry <harshilmistry31@gmail.com>
2025-08-13 16:11:08 +0530

    Minor changes

--- lib/inspo_main.dart
+++ lib/inspo_main.dart
@@ -32,159 +32,185 @@ class _TodoScreenState extends State<TodoScreen> {
           shadowColor: Colors.black,
           elevation: 5,
         ),
-        body: Center(
-          child: Column(
-            children: [
-              // TextField with original styling
-              TextField(
-                controller: tec1,
-                onChanged: (value) => updateTodo(value),
-                decoration: InputDecoration(
-                  hintText: isEditing ? 'Edit your todo...' : 'Add a new todo...',
-                ),
-              ),
-              Padding(padding: EdgeInsets.all(10)),
-              
-              // Enhanced Buttons Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => isEditing ? saveEdit() : addTodo(),
-                    child: Text(isEditing ? "Save Edit" : "Add To-do"),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.pink),
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                    ),
+        //Basic Padding
+        body: Padding(
+          padding: EdgeInsetsGeometry.all(15),
+          child: Center(
+            child: Column(
+              children: [
+
+                //Input Field for the todos
+                TextField(
+                  controller: tec1,
+                  onChanged: (value) => updateTodo(value),
+                  decoration: InputDecoration(
+                    hintText: isEditing
+                        ? 'Edit your todo...'
+                        : 'Add a new todo...',
                   ),
-                  const SizedBox(width: 35),
-                  if (isEditing)
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+
+                //Padding between buttons and Input Filed
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
                     ElevatedButton(
-                      onPressed: () => cancelEdit(),
-                      child: Text("Cancel"),
+                      onPressed: () => isEditing ? saveEdit() : addTodo(),
+                      child: Text(isEditing ? "Save Edit" : "Add To-do"),
                       style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.grey),
-                        foregroundColor: MaterialStateProperty.all(Colors.white),
+                        backgroundColor: MaterialStateProperty.all(Colors.pink),
+                        foregroundColor: MaterialStateProperty.all(
+                          Colors.white,
+                        ),
                       ),
                     ),
-                  if (!isEditing)
-                    ElevatedButton(
-                      onPressed: () => resetTodo(),
-                      child: Text("Reset To-do"),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.white),
-                        foregroundColor: MaterialStateProperty.all(Colors.red),
-                      ),
-                    ),
-                ],
-              ),
-              Padding(padding: EdgeInsets.all(10)),
-              Text(str, style: TextStyle(color: Colors.pink)),
-              Padding(padding: EdgeInsets.all(10)),
-              
-              // Enhanced Todo List
-              Expanded(
-                child: Card(
-                  child: todos.isEmpty
-                      ? Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.task_alt,
-                                size: 64,
-                                color: Colors.grey[400],
-                              ),
-                              const SizedBox(height: 16),
-                              Text(
-                                "No todos yet!",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.grey[600],
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              Text(
-                                "Add your first todo above",
-                                style: TextStyle(
-                                  color: Colors.grey[500],
-                                ),
-                              ),
-                            ],
+                    //For adding some space between Buttons
+                    SizedBox(width: 35),
+                    if (isEditing)
+                      ElevatedButton(
+                        onPressed: () => cancelEdit(),
+                        child: Text("Cancel"),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.grey,
                           ),
-                        )
-                      : ListView.builder(
-                          itemCount: todos.length,
-                          itemBuilder: (context, index) {
-                            final todo = todos[index];
-                            final isImportant = todo['important'] ?? false;
-                            final isCompleted = todo['completed'] ?? false;
-                            
-                            return Card(
-                              child: Padding(
-                                padding: EdgeInsets.all(5),
-                                child: ListTile(
-                                  leading: GestureDetector(
-                                    onTap: () => toggleComplete(index),
-                                    child: Icon(
-                                      isCompleted ? Icons.check_circle : Icons.check_circle_outline,
-                                      color: isCompleted ? Colors.green : Colors.pink,
+                          foregroundColor: MaterialStateProperty.all(
+                            Colors.white,
+                          ),
+                        ),
+                      ),
+                    if (!isEditing)
+                      ElevatedButton(
+                        onPressed: () => resetTodo(),
+                        child: Text("Reset To-do"),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.white,
+                          ),
+                          foregroundColor: MaterialStateProperty.all(
+                            Colors.red,
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(str, style: TextStyle(color: Colors.pink)),
+                Padding(padding: EdgeInsets.all(10)),
+
+                // Actual Todo List
+                Expanded(
+                  child: Card(
+                    child: todos.isEmpty
+                        //In case no todos are added yet
+                        ? Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.task_alt,
+                                  size: 64,
+                                  color: Colors.grey[400],
+                                ),
+                                const SizedBox(height: 16),
+                                Text(
+                                  "No todos yet!",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey[600],
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  "Add your first todo above",
+                                  style: TextStyle(color: Colors.grey[500]),
+                                ),
+                              ],
+                            ),
+                          )
+
+                        //When todos are added
+                        : ListView.builder(
+                            itemCount: todos.length,
+                            itemBuilder: (context, index) {
+                              final todo = todos[index];
+                              final isImportant = todo['important'] ?? false;
+                              final isCompleted = todo['completed'] ?? false;
+
+                              return Card(
+                                child: Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: ListTile(
+                                    leading: GestureDetector(
+                                      onTap: () => toggleComplete(index),
+                                      child: Icon(
+                                        isCompleted
+                                            ? Icons.check_circle
+                                            : Icons.check_circle_outline,
+                                        color: isCompleted
+                                            ? Colors.green
+                                            : Colors.pink,
+                                      ),
+                                    ),
+                                    title: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            todo['text'],
+                                            style: TextStyle(
+                                              decoration: isCompleted
+                                                  ? TextDecoration.lineThrough
+                                                  : null,
+                                              color: isCompleted
+                                                  ? Colors.grey[500]
+                                                  : Colors.black,
+                                              fontWeight: isImportant
+                                                  ? FontWeight.bold
+                                                  : FontWeight.normal,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        TextButton(
+                                          onPressed: () =>
+                                              toggleImportant(index),
+                                          child: Icon(
+                                            isImportant
+                                                ? Icons.star
+                                                : Icons.star_border,
+                                            color: Colors.orange,
+                                          ),
+                                        ),
+                                        TextButton(
+                                          onPressed: () => startEdit(index),
+                                          child: Icon(
+                                            Icons.edit,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                        TextButton(
+                                          onPressed: () => deleteTodo(index),
+                                          child: Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  title: Row(
-                                    children: [
-                                      if (isImportant)
-                                        Padding(
-                                          padding: EdgeInsets.only(right: 8),
-                                          child: Icon(
-                                            Icons.star,
-                                            color: Colors.orange,
-                                            size: 20,
-                                          ),
-                                        ),
-                                      Expanded(
-                                        child: Text(
-                                          todo['text'],
-                                          style: TextStyle(
-                                            decoration: isCompleted ? TextDecoration.lineThrough : null,
-                                            color: isCompleted ? Colors.grey[500] : Colors.black,
-                                            fontWeight: isImportant ? FontWeight.bold : FontWeight.normal,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  trailing: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      TextButton(
-                                        onPressed: () => toggleImportant(index),
-                                        child: Icon(
-                                          isImportant ? Icons.star : Icons.star_border,
-                                          color: Colors.orange,
-                                        ),
-                                      ),
-                                      TextButton(
-                                        onPressed: () => startEdit(index),
-                                        child: Icon(
-                                          Icons.edit,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                      TextButton(
-                                        onPressed: () => deleteTodo(index),
-                                        child: Icon(Icons.delete, color: Colors.red),
-                                      ),
-                                    ],
-                                  ),
                                 ),
-                              ),
-                            );
-                          },
-                        ),
+                              );
+                            },
+                          ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -194,7 +220,7 @@ class _TodoScreenState extends State<TodoScreen> {
   void resetTodo() {
     setState(() {
       todos = [];
-      str = 'All todos cleared!';
+      str = 'Bus badhu khatam have 💔';
     });
   }
 
@@ -215,16 +241,12 @@ class _TodoScreenState extends State<TodoScreen> {
     var todo = tec1.text;
     if (todo == '') {
       setState(() {
-        str = 'Please enter a todo first!';
+        str = 'Gandu todo nakh pehla';
       });
     } else {
       setState(() {
         str = 'Todo added successfully!';
-        todos.add({
-          'text': todo,
-          'important': false,
-          'completed': false,
-        });
+        todos.add({'text': todo, 'important': false, 'completed': false});
         tec1.clear();
       });
     }
@@ -235,7 +257,7 @@ class _TodoScreenState extends State<TodoScreen> {
       isEditing = true;
       editingIndex = index;
       tec1.text = todos[index]['text'];
-      str = 'Editing todo...';
+      str = 'Party badal liya sala';
     });
   }
 
@@ -243,7 +265,7 @@ class _TodoScreenState extends State<TodoScreen> {
     var todo = tec1.text;
     if (todo.isEmpty) {
       setState(() {
-        str = 'Please enter a todo first!';
+        str = 'Gandu To-do nakh pehla!';
       });
     } else if (editingIndex != null) {
       setState(() {
@@ -251,7 +273,7 @@ class _TodoScreenState extends State<TodoScreen> {
         isEditing = false;
         editingIndex = null;
         tec1.clear();
-        str = 'Todo updated successfully!';
+        str = 'Done che bhai!';
       });
     }
   }
@@ -261,25 +283,25 @@ class _TodoScreenState extends State<TodoScreen> {
       isEditing = false;
       editingIndex = null;
       tec1.clear();
-      str = 'Edit cancelled';
+      str = 'Bus ne bhai, cancel kari didhu';
     });
   }
 
   void toggleImportant(int index) {
     setState(() {
       todos[index]['important'] = !todos[index]['important'];
-      str = todos[index]['important'] 
-          ? 'Todo marked as important!' 
-          : 'Todo removed from important!';
+      str = todos[index]['important']
+          ? 'Todo is def imp'
+          : 'Chal hatt b.k.l...';
     });
   }
 
   void toggleComplete(int index) {
     setState(() {
       todos[index]['completed'] = !todos[index]['completed'];
-      str = todos[index]['completed'] 
-          ? 'Todo completed!' 
-          : 'Todo marked as incomplete!';
+      str = todos[index]['completed']
+          ? 'Hais Patyu'
+          : 'Not yet brother';
     });
   }
 }
